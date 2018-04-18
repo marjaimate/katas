b81b62c3fb8117375c9c818d7bf55572dd411378
Lasse Bach <lab@issuu.com>
2018-04-18 13:20:00 +0200

    Create luhn_lasse_susana.ex

---
+++ solutions/luhn_lasse_susana.ex
@@ -0,0 +1,24 @@
+defmodule Luhn do
+    def validate(l) when length(l) == 16 do
+        rem(checksum(l), 10) == 0
+    end
+    def checksum(l) do
+        [h | t] = Enum.reverse(l)
+        sanitize_list = t
+        |> Enum.map_every(2, fn x -> x*2 end)
+        |> Enum.map(&special_sum/1)
+        
+        h + calc(sanitize_list)
+    end
+    def calc([n1]) do
+        n1
+    end
+    def calc([n1, n2 | t]) do
+        n1 + n2 + calc(t)
+    end
+
+    def special_sum(s) when s < 10, do: s
+    def special_sum(s) do
+        Integer.digits(s, 10) |> Enum.sum
+    end
+end
